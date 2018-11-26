d4d97edc88c98cb06c9e39aa26a411b8f46f5bd3
BrentBauman86 <brentbauman86@gmail.com>
2018-11-26 12:45:28 -0800

    add toy index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,17 @@ class UsersController < ApplicationController
     end
     
     def create
-      @user = User.new(params[:user])
+      @user = User.new(user_params)
       if @user.save
         redirect_to root_url, notice: "Thank you for signing up!"
       else
         render "new"
       end
     end
+
+    private 
+
+      def user_params
+        params.require(:user).permit(:name, :password_digest, :work_ethic, :admin)
+      end
   end
