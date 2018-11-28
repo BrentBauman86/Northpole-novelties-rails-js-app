ad1ff02feca039a04c15e20cc6917b5c8a27155e
BrentBauman86 <brentbauman86@gmail.com>
2018-11-28 08:21:11 -0800

    refine user error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-   has_secure_password 
-   has_many :categoies, through: :toys 
-   has_many :toys 
+    has_secure_password 
+    validates_presence_of :name, :password
+    validates_length_of :password, in: 5..30
    
-   validates_uniqueness_of :name
+    has_many :categoies, through: :toys 
+    has_many :toys 
 
 end 
