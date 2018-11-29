2ab13fb4c888837b4f75c3a9d339c6454e2530bc
BrentBauman86 <brentbauman86@gmail.com>
2018-11-28 19:03:58 -0800

    built create category method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,6 +9,17 @@ class CategoriesController < ApplicationController
     end
 
     def create
-        
+        @category = Category.new(category_params) 
+        if @category.save 
+            redirect_to categories_path 
+        else
+            redirect_to categories_new_path
+        end 
+    end
+
+    private 
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
