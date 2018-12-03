47b4e5446ffa108e10ff59d46343e553add885e4
BrentBauman86 <brentbauman86@gmail.com>
2018-12-02 19:02:45 -0800

    build create method and view for toys

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,5 @@
 class ToysController < ApplicationController 
-    before_action :authorize, only: [:edit, :update]
+    before_action :authorize, only: [:edit, :update, :create]
 
     def index
         @toys = Toy.all 
@@ -10,6 +10,17 @@ class ToysController < ApplicationController
     end
 
     def create 
+        @toy = Toy.new(toy_params)
+        if @toy.save 
+            redirect_to category_toys_path 
+        else
+            render "new"
+        end  
     end
 
+    private 
+
+    def toy_params
+        params.require(:toy).permit(:name, :materials, :quantity, :rating)
+    end 
 end
