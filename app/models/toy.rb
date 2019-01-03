9fc0d6488beef6d2aa4aa42812779a597203f5f5
BrentBauman86 <brentbauman86@gmail.com>
2019-01-03 06:12:42 -0800

    implement scope method and delete build category link

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -2,7 +2,7 @@ class Toy < ActiveRecord::Base
     has_and_belongs_to_many :categories 
     belongs_to :user, optional: true 
     validates :name, :rating, presence: true 
-    scope :rating, -> { where('rating > 7') }
+    scope :high_rating, -> { where('rating > 7') }
 
     accepts_nested_attributes_for :categories, allow_destroy: true
 end
