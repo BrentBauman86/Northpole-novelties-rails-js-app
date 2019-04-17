38f0434edbe58e92697711d7741c62c6ec383f6d
BrentBauman86 <brentbauman86@gmail.com>
2019-04-17 04:08:04 -0700

    generate User serializer

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -8,7 +8,7 @@ class ToysController < ApplicationController
            @toys = Toy.all 
         end
 
-        response_to do |f|
+        respond_to do |f|
             f.html {render :index}
             f.json {render json: @toys}
         end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,5 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :password, :admin, :uid  
+
+  has_many :toys
+end
