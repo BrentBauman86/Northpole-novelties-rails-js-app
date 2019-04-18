c2f8f652608ba476a3840733ff08459d3476f96e
BrentBauman86 <brentbauman86@gmail.com>
2019-04-18 04:49:15 -0700

    build ajax request along with class constructor

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,32 @@
 $(function() {
-    alert("we in?!")
+    prompt("we in?!")
 });
+
+function getUsers() {
+    $.ajax({
+        url: 'http://localhost:3000/users',
+        method: 'get',
+        dataType: 'json'
+    }).done(function (data) {
+        console.log('the data is:', data)
+        let user = User.all 
+        let userHTML = user.showHTML()
+        document.getElementById('users').innerHTML = userHTML
+    })
+}
+
+class User {
+    constructor(attr) {
+        this.id = attr.id 
+        this.name = attr.name 
+        this.password = attr.password
+    }
+}
+
+User.prototype.showHTML = function() {
+    return (
+        <div>
+          <p>${this.name}</p>
+        </div>
+    )
+}
