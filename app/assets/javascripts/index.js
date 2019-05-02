02438ba32055d84f620749f67c4e2ea55ae8fbec
BrentBauman86 <brentbauman86@gmail.com>
2019-05-02 02:04:21 -0700

    trying to create a link using toy name to direct user to show page

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,18 +1,37 @@
 $(function() {
     console.log("index.js is loaded...")
     listenForClick();
+    showToyClick();
 });
 
-function getToys() {
+function getToyList() {
     $.getJSON(`/toys`, function (data) {
         let toyList = "";
+      
         data.forEach(function(toys){
             let toy = new Toy(toys);
             let toyHTML = toy.showHTML();
             toyList += toyHTML 
         });
+
+        let table = document.getElementById('toy-table');
+        table += toyTable();
         let toyData = document.getElementById('toys');
         toyData.innerHTML = toyList;
+    });
+}
+//using ajax request for getToyList was causing a cross-origin request error
+
+function getToy() {
+    let catId = $(".toy-details-js").attr("data-cat-id")
+    let toyId = $(".toy-details-js").attr("data-toy-id")
+    $.getJSON(`/categories/${catId}/toys/${toyId}`, function(data){
+        console.log('ill take that...')
+        let toy = new Toy(data);
+        let toyDetails = toy.showHTML();
+
+        let toyData = document.getElementById('show-toy')
+        toyDetails.toyData.innerHTML 
     });
 }
 
@@ -27,22 +46,51 @@ class Toy {
     }
 }
 
+ function toyTable() {
+return (`
+    <table class="table">
+        
+     <tr>
+         <td>Name</td>
+         <td>Quantity</td>
+         <td>Rating</td>
+         <td>Edit</td>
+        </tr>
+        </table>
+`)
+}
+
 Toy.prototype.showHTML = function() {
     return (`
-        <div>
-          <p>${this.name}</p>
-          <p>${this.quantity}</p>
-          <p>${this.rating}</p>
-          <p>${this.user_id}</p>
-          <p>${this.category_id}</p>
-          <p>${this.id}</p>
-        </div>
+    <ul>
+    <table class="table">
+        
+     <tr>
+         <td>Name</td>
+         <td>Quantity</td>
+         <td>Rating</td>
+         <td>Edit</td>
+        </tr>
+         <tr>
+             <td><li>${this.name}</li></td>             
+             <td>${this.quantity}</td>
+             <td>${this.rating}</td>
+        </tr>
+    </table>
+    </ul>
     `)
 }
 
 function listenForClick() {
     $('button#toy-data').on("click", function(e) {
         e.preventDefault()
-        getToys();
+        getToyList();
+    })
+}
+
+function showToyClick() {
+    $('button#toy-details').on("click", function(e) {
+        e.preventDefault()
+        getToy();
     })
 }
